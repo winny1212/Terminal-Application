d9340f8b99164055893bf4dc2a289ccb55010d67
Yujing Chen <yujingchen@192-168-1-3.tpgi.com.au>
2021-05-26 10:01:12 +1000

    add the exit_program method to the file

--- board_style.rb
+++ board_style.rb
@@ -3,3 +3,23 @@ def greeting
     speech.speak
     puts "Hi,welcome to the luxury boarding home"
 end
+
+def menu
+    puts "Do you want book a dog boarding position?".colorize(:blue)
+    puts "1: Yes. I want to book a position.".colorize(:blue)
+    puts "2: No. Just want to view some information.".colorize(:blue)
+    puts "3: Exit"
+    puts
+    print">"
+end
+
+def exit_program
+    puts "Are you sure to quit? (Y/N)"
+    quit_choice = gets.chomp.upcase
+    if quit_choice == "Y"
+        puts "Thank you for using!"
+        exit
+    else
+        menu
+    end
+end
